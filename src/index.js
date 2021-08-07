217a664a2edcf4904bbec8a4a2e5f9175fdc55ed
Benis Tambe <will.jenifer@yahoo.com>
2021-08-07 13:57:46 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const  button = document.querySelector("[type='submit']");
+  const tasks = document.getElementById("tasks");
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    const item = document.getElementById('new-task-description').value;
+    const li = document.createElement("li");
+    const newItem = document.createTextNode(item);
+    li.appendChild(newItem);
+    tasks.appendChild(li);
+    document.getElementById('new-task-description').value = "";
+  })
+  
 });
